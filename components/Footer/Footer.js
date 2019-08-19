2ba63ca532ef0b9b580ccfdc8c83882450f45453
Alejandro Nanez <alejonanez@gmail.com>
2019-08-19 14:16:47 -0500

    Add footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import Styles from './Footer.css';
+
+const Footer = () => (
+  <footer className={Styles.container}>2019 - Hecho con ❤️ en Medellín</footer>
+);
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import SignupForm from '../components/SignupForm';
 import Intro from '../components/Intro';
+import Footer from '../components/Footer';
 
 const Home = () => (
-  <main style={{ height: '100%' }}>
-    <Intro />
-    <SignupForm />
-  </main>
+  <>
+    <main style={{ height: '100%', minHeight: '100%' }}>
+      <Intro />
+      <SignupForm />
+    </main>
+    <Footer />
+  </>
 );
 
 export default Home;

576ca12617c871792a883efb0c1c949b66d9ad7e
Alejandro Nanez <alejonanez@gmail.com>
2019-08-19 13:54:12 -0500

    Add form

--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -1,12 +1,12 @@
 import Styles from './Intro.css';
 
 const Intro = () => (
-  <section>
+  <section className={Styles.container}>
     <h1 className={Styles.title}>React La Conferencia</h1>
-    <p className={Styles.subTitle}>
+    <h2 className={Styles.subTitle}>
       La primera conferencia sobre React y su ecosistema para hispanoparlantes
       en América
-    </p>
+    </h2>
   </section>
 );
 
--- components/SignupForm/SignupForm.js
+++ components/SignupForm/SignupForm.js
@@ -1,3 +1,21 @@
-const SignupForm = () => <form>Signup form</form>;
+import React from 'react';
+import Styles from './SignupForm.css';
+
+const SignupForm = () => (
+  <form className={Styles.container}>
+    <label className={Styles.label} htmlFor="email">
+      Inscribe tu correo para enterarte de nuetro CFP, venta de tickets y
+      anuncios!
+    </label>
+    <input
+      className={Styles.input}
+      type="email"
+      id="email"
+      placeholder="tucorreo@gmail.com"
+    />
+
+    <input className={Styles.cta} type="submit" value="Inscríbete" />
+  </form>
+);
 
 export default SignupForm;
